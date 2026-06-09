9a67a3eb818099780bf3ab287942103bfc1041e1
Marcotian0211 <350327743@tdsb.ca>
2026-06-09 15:36:27 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,73 +4,154 @@ import java.util.Random;
 public class PracticeProblem {
 
     public static void main(String[] args) {
-	Scanner scanner=new Scanner(System.in);
-    Random random=new Random();
-String word;
-int number=random.nextInt(128);
-int number1=random.nextInt(128);
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String word;
+        String word1;
+int number=random.nextInt(128);//player number
+int number1=random.nextInt(128);//bot number
 int number2;
-int number3=random.nextInt(100);
-int number4=random.nextInt(100);//bot number
-int number5=random.nextInt(100);
+int number3;
+int number4=128;
 int range=0;
 int skip=0;
 int confuse=0;
-String word1;
-        System.out.println("Welcome to the Small Card Game");
-        System.out.println("The rule of the game:");
-        System.out.println("This is a bot vs human game. You and the bot will get a random number.");
-        System.out.println("If you guess the bot's number correctly, you win.");
-        System.out.println("If the bot guesses your number first, the bot wins.");
-        System.out.println("do you want to play game?");
-		word=scanner.nextLine();
-		String word1=word.toLowerCase();
-		while(word1!="yes"||word1!="no"){System.out.println("please enter \"yes\"or \"no\"");
-    }
-if (word1=="no"){
-System.out.print("have a good day");
-if(word1=="yes"){ System.out.println("your number is "+number);
+int botLow=0;
+int botHigh=100;
+    System.out.println("Welcome to the Small Card Game");
+       System.out.println("The rule of the game:");
+       System.out.println("This is a bot vs human game. You and the bot will get a random number.");
+       System.out.println("If you guess the bot's number correctly, you win.");
+       System.out.println("If the bot guesses your number first, the bot wins.");
+       System.out.println("do you want to play game?");
 
-    do{
-        System.out.println("Please guess enter a number:");
-        number2=scanner.nextInt();
-        if(scanner.hasNextInt()&&number2!=number1){
-            System.out.println("You guess wrong");
-          if(number3<=50){
-            System.out.print("you get range card");
-            range++;
-          }
-          if(number3<=76){
-           System.out.print("you get skip card");
-           skip++;
-          }
-          if(number3<=100){
-            System.out.print("you get confuse card");
-            confuse++;
-          }
-          System.out.print("would you want to use the which card?");
-           word1=scanner.nextLine();
-         if(word=="range card"){
-         System.out.println("the range number of the oppensent is:"+number1/2);
-         range--;
-         }
-         if(word=="skip card"&&skip>=2){
-            System.out.println("the bot round was skip.");
-            skip=skip-2;
-           }
-           if(word=="confuse"){
-            System.out.println("the bot get the wrong range of number");
-            confuse--;
-           }
-            
-        
+
+        System.out.println("Do you want to play? (yes/no)");
+        word1 = scanner.nextLine();
+        word1=word1.toLowerCase();
+        while (!word1.equals("yes")
+                && !word1.equals("no")) {
+
+            System.out.println("Please enter yes or no:");
+            word1 = scanner.nextLine();
         }
-    
-        }while(number2==number4);
-        
-    }
+
+        if (word1.equals("no")) {
+            System.out.println("Have a good day!");
+            return;
+        }
+        System.out.println("Your number is: " + number);
+        do {
+
+            System.out.println();
+            System.out.println("Please guess the bot's number:");
+            number2 = scanner.nextInt();
+            if (number2 == number1) {
+
+                System.out.println("You guessed correctly!");
+                System.out.println("YOU WIN!");
+                break;
+            }
+
+            System.out.println("You guessed wrong.");
+
+            number3 = random.nextInt(100) + 1;
+
+            if (number3 <= 50) {
+
+                System.out.println("You got a Range Card.");
+                range++;
+            } else if (number3>=50&&number3<=76) {
+
+                System.out.println("You got a Skip Card.");
+                skip++;
+
+            } else {
+
+                System.out.println("You got a Confuse Card.");
+                confuse++;
+            }
+
+            System.out.println();
+            System.out.println("Range Cards: " + range);
+            System.out.println("Skip Cards: " + skip);
+            System.out.println("Confuse Cards: " + confuse);
+
+            scanner.nextLine();
+
+            System.out.println(
+                    "Which card do you want to use? (range/skip/confuse/none)");
+
+            word = scanner.nextLine();
+
+            if (word.equals("range") && range > 0) {
+if(number1>=64){
+                System.out.println( "The bot's number is larger than " + (64));
+if(number1<64){ System.out.println( "The bot's number is lower than " + (64));
 
 }
+                range--;}
+            }
+                   if (word.equals("range 2") && range > 0){if(number1>=64&&number1<=96){
+                System.out.println( "The bot's number is between 64 to96");
+               if(number1<64&&number1>=32)
+                System.out.println( "The bot's number is between 32 to 64");}
+                range=range-2; }
+                
+            else if (word.equals("skip") && skip > 0) {
 
+                System.out.println("Bot turn skipped.");
+                skip--;
+                continue;
+            }
+
+            else if (word.equals("confuse") && confuse > 0) {
+
+                System.out.println("Bot is confused and will guess randomly.");
+
+                confuse--;
+
+                int botGuess = random.nextInt(128);
+
+                System.out.println(
+                        "Bot guesses: " + botGuess);
+
+                if (botGuess == number) {
+
+                    System.out.println(
+                            "Bot guessed your number!");
+                    System.out.println("BOT WINS!");
+                    break;
+                }
+
+                continue;
+            }
+
+            int botGuess = (botLow + botHigh) / 2;
+
+            System.out.println();
+            System.out.println(
+                    "Bot guesses: " + botGuess);
+
+            if (botGuess == number) {
+
+                System.out.println(
+                        "Bot guessed your number!");
+
+                System.out.println("BOT WINS!");
+                break;
+            }
+
+            if (botGuess < number) {
+
+                botLow = botGuess + 1;
+
+            } else {
+
+                botHigh = botGuess - 1;
+            }
+
+        } while (true);
     }
 }
